3ad329fabe55c00dd2d0a8099545008ef32e6139
haurog <36535774+haurog@users.noreply.github.com>
2022-06-08 20:19:34 +0200

    added number counter

--- example.js
+++ example.js
@@ -274,6 +274,42 @@ async function onDisconnect() {
   document.querySelector("#connected").style.display = "none";
 }
 
+function decrement(e) {
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
+  );
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value--;
+  target.value = value;
+}
+
+function increment(e) {
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
+  );
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value++;
+  target.value = value;
+}
+
+const decrementButtons = document.querySelectorAll(
+  `button[data-action="decrement"]`
+);
+
+const incrementButtons = document.querySelectorAll(
+  `button[data-action="increment"]`
+);
+
+decrementButtons.forEach(btn => {
+  btn.addEventListener("click", decrement);
+});
+
+incrementButtons.forEach(btn => {
+  btn.addEventListener("click", increment);
+});
+
 
 /**
  * Main entry point.
