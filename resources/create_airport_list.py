2bb9b0983532b7fa25184d3967427149d3466b31
haurog <36535774+haurog@users.noreply.github.com>
2022-06-19 13:45:02 +0200

    improve airport output formating

--- resources/create_airport_list.py
+++ resources/create_airport_list.py
@@ -25,6 +25,8 @@ with open(INPUT_FILE, mode='r')as input_file:
                            lines[10] + " " + \
                            lines[8] + " " + \
                            lines[13] # name + municipality + iso country + Iata
+            airport_name = airport_name.replace("'", "\'")
+            airport_name = airport_name.replace('"', '\"')
             airport_name = airport_name.strip()
             temp_airport = [airport_name,
                             lines[4],
@@ -39,7 +41,7 @@ with open(INPUT_FILE, mode='r')as input_file:
 print("Airports selected: " + str(number_of_airports))
 
 with open(OUTPUT_FILE, mode='w')as out_file:
-    out_file.write('let airports = [')
+    out_file.write('const airports = [\n')
     for airport in airports_selected:
         out_file.write('["'+airport[0]+'"')
         out_file.write(', ' + airport[1])
@@ -48,4 +50,4 @@ with open(OUTPUT_FILE, mode='w')as out_file:
         if airport != airports_selected[-1]:  # don't add a comma to the last one
             out_file.write(",")
         out_file.write("\n")
-    out_file.write(']\n')
+    out_file.write('];\n')
